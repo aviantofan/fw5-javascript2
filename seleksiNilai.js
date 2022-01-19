6f6acba5eec4a0d3019027b787f8d12742722102
eexxprd <aviantofan@gmail.com>
2022-01-19 09:14:45 +0700

    Solving error

--- seleksiNilai.js
+++ seleksiNilai.js
@@ -1,10 +1,11 @@
 function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray){
 
-    if (nilaiAwal > nilaiAkhir) {
-        console.log("Nilai akhir harus lebih besar dari nilai awal")
-        } if (dataArray.length < 5) {
-            console.log("Jumlah angka dalam dataArray harus lebih dari 5")
-    }else{
+    if (nilaiAwal > nilaiAkhir) { 
+        return("Nilai akhir harus lebih besar dari nilai awal") 
+    } 
+    if (dataArray.length < 5) { 
+        return("Jumlah angka dalam dataArray harus lebih dari 5") 
+    }else{ 
         const fin = dataArray.filter(function(num){
             return num > nilaiAwal && num < nilaiAkhir
         })
@@ -14,4 +15,4 @@ function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray){
     return seleksiNilai
 }
 
-console.log(seleksiNilai(5,20,[2,25,4,14,17,30,8]))
+console.log(seleksiNilai(5,20,[2, 25, 4, 14, 17, 30, 8]))
