db3d5f747b06cb41c177a58d97c0192f32a12055
eexxprd <aviantofan@gmail.com>
2022-01-19 14:52:01 +0700

    Solving error

--- searchingName.js
+++ searchingName.js
@@ -1,11 +1,16 @@
 const name = ["Abigail", "Alexandra", "Alison", "Amanda", "Angela", "Bella", "Carol", "Caroline", "Carolyn", "Deirdre", "Diana", "Elizabeth","Ella", "Faith", "Olivia", "Penelope"]
 
-function searchName(arr, query) {
-    return arr.filter(function(a) {
-      return a.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    })
-  }
-  
-  name.splice(5)
-  
-  console.log(searchName(name, "an"))
+function searchName(a,cb){
+  console.log(a)
+  return cb(name)  
+}
+
+function cari(name){
+  const fin = name.filter(function(arr){
+    return arr.toLowerCase().includes("an")
+  })
+    fin.splice(3)
+    return fin
+}
+
+console.log(searchName("OUTPUT : ", cari))
